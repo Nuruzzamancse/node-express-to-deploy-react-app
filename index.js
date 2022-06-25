17bb23395f6651a90bc8a4cf1eaee4082f60f615
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2022-06-25 11:08:38 +0600

    order changed

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+app.get('/test', (req, res) => {
+  res.send('Hello World!')
+})
 
 // Make build folder public
 app.use(express.static('./demo/build'));
@@ -10,10 +13,6 @@ app.use(express.static('./demo/build'));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './demo/build', 'index.html'));
 })
-
-app.get('/test', (req, res) => {
-    res.send('Hello World!')
-  })
 
 
 app.listen(port, () => {
